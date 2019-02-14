3e0f45d43e191dbfb8579cd05403f60857aa7559
meijie <656983975@qq.com>
2019-02-14 22:11:02 +0800

    'lianxi'

--- dome/fs_readFile.js
+++ dome/fs_readFile.js
@@ -1,11 +1,24 @@
+// const fs = require('fs');
+
+// fs.readFile('test.txt','utf-8',(data,err)=>{
+//     if(data){
+//         console.log(data);
+//     }else{
+//         console.log(err)
+//     }
+// })
+
+// //fs 读文件
+
 const fs = require('fs');
 
-fs.readFile('test.txt','utf-8',(data,err)=>{
-    if(data){
-        console.log(data);
-    }else{
-        console.log(err)
+fs.readFile(
+    './test.txt',
+    'utf-8',
+    (data,err) => {
+        data ?
+            console.log(data)
+            :
+            console.log(err);
     }
-})
-
-//fs 读文件
+);
